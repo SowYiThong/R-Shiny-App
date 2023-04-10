4b15d4a7740824688df670414b457e2452786378
SowYiThong <yithong.sow@n-actuarial.com>
2023-04-10 12:58:57 +0800

    uploaded the ui and server separately

---
+++ UI.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+ui <- fluidPage(
+  "Cumulative Paid Claims", 
+  fileInput("upload", 
+            label = "Upload one CSV or Excel file", 
+            multiple = FALSE, 
+            accept = c(".xlsx",".csv"), 
+            buttonLabel = "Upload"),
+  dataTableOutput("original_table"), 
+  sliderInput("slider", 
+              label = "Tail Factor:",
+              min = 0,
+              max = 2,
+              value = 1.1,
+              step = 0.1,),
+  verbatimTextOutput("slider_output"), 
+  dataTableOutput("results_table"), 
+  plotOutput("cumulative_plot")
+)
